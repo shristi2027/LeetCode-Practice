5478a8c3404d7454a2abf76a2be6065e16458937
shristi2027 <150129610+shristi2027@users.noreply.github.com>
2026-04-07 11:06:22 +0530

    Create solution.py

---
+++ LongestSubstring/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        charSet = set()
+        left = 0
+        result = 0
+
+        for right in range (len(s)):
+            while s[right] in charSet:
+                charSet.remove(s[left])
+                left += 1
+
+            charSet.add(s[right])
+            result = max(result, right-left+1)
+        return result
